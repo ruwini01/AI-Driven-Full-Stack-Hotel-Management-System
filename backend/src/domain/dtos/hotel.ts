c33e3db95b27104070683009db2bc0b966f20a54
ruwini01 <12345tharanga12345@gmail.com>
2025-12-05 10:41:27 +0530

    update CreateHotelDTO to fit all fields

--- backend/src/domain/dtos/hotel.ts
+++ backend/src/domain/dtos/hotel.ts
@@ -2,11 +2,16 @@ import { z } from "zod";
 
 export const CreateHotelDTO = z.object({
   name: z.string(),
-  image: z.string(),
   location: z.string(),
-  price: z.number(),
+  city: z.string(),
+  rating: z.number().min(0).max(5).optional(),
   description: z.string(),
+  price: z.number(),
+  stars: z.number().min(1).max(5),
+  amenities: z.array(z.string()).optional(),
+  images: z.array(z.string()).nonempty("At least one image is required"),
 });
+
 
 export const SearchHotelDTO = z.object({
   query: z.string().min(1),
