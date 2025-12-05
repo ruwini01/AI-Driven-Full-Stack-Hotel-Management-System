ac615d75d4e7be78808bcf7fc2f2b702b8e44dfc
ruwini01 <12345tharanga12345@gmail.com>
2025-12-05 15:56:05 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/api/",
+    baseUrl: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/",
     prepareHeaders: async (headers) => {
       return new Promise((resolve) => {
         async function checkToken() {
